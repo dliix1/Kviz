19e78be8b8cb324478f77a476d1b2c2d755411d9
Zuhdija Delic <zuka_delic@hotmail.com>
2019-04-19 19:39:34 +0200

    bla

--- Ispis.java
+++ Ispis.java
@@ -1,5 +1,16 @@
 package Mathquiz;
 
 public class Ispis {
+	
+	public void ispis(){
+
+		System.out.println("Odaberite radnju koju zelite za kviz: ");
+		System.out.println("1. Sabiranje ");
+		System.out.println("2. Oduzimanje ");
+		System.out.println("3. Mnozenje ");
+		System.out.println("4. Dijeljenje ");
+		System.out.println("5. Korjenovanje ");
+		System.out.println("6. Kvadriranje ");
+	}
 
 }
