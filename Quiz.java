63cb4af8a13162aebf7fcd749fae7410ad641fc3
Zuhdija Delic <zuka_delic@hotmail.com>
2019-04-19 19:35:13 +0200

    Ispis dodan, zavrseno sve

--- Quiz.java
+++ Quiz.java
@@ -2,4 +2,97 @@ package Mathquiz;
 
 public class Quiz {
 
+	double broj1;
+	double broj2;
+
+	public double getBroj1() {
+		return broj1;
+	}
+
+	public void setBroj1(double broj1) {
+		this.broj1 = broj1;
+	}
+
+	public double getBroj2() {
+		return broj2;
+	}
+
+	public void setBroj2(double broj2) {
+		this.broj2 = broj2;
+	}
+
+	double rezultat;
+
+	public double getRezultat() {
+		return rezultat;
+	}
+
+	Quiz() {
+
+	}
+
+	public void sabiranje() {
+		broj1 = (int) (Math.random() * 10);
+		broj2 = (int) (Math.random() * 10);
+
+		System.out.println("Koliko je " + broj1 + " + " + broj2 + "?");
+
+		rezultat = broj1 + broj2;
+
+	}
+
+	public void oduzimanje() {
+		broj1 = (int) (Math.random() * 10);
+		broj2 = (int) (Math.random() * 10);
+
+		System.out.println("Koliko je " + broj1 + " - " + broj2 + "?");
+
+		rezultat = broj1 - broj2;
+
+		if (broj1 < broj2) {
+			System.out.println("Koliko je " + broj2 + " - " + broj1 + "?");
+			rezultat = broj2 - broj1;
+
+		}
+
+	}
+
+	public void mnozenje() {
+		broj1 = (int) (Math.random() * 10);
+		broj2 = (int) (Math.random() * 10);
+
+		System.out.println("Koliko je " + broj1 + " * " + broj2 + "?");
+
+		rezultat = broj1 * broj2;
+	}
+
+	public void dijeljenje() {
+		broj1 = (int) (Math.random() * 10);
+		broj2 = (int) (Math.random() * 10);
+
+		if (broj1 % broj2 == 0) {
+			System.out.println("Koliko je " + broj1 + " / " + broj2 + "?");
+
+			rezultat = broj1 / broj2;
+		}
+	}
+
+	public void korjenovanje() {
+		broj1 = (int) (Math.random() * 10);
+		broj2 = Math.pow(broj1, 2);
+
+		System.out.println("Koji je korijen iz " + broj2 + "?");
+
+		rezultat = Math.sqrt(broj2);
+
+	}
+
+	public void kvadriranje() {
+		broj1 = (int) (Math.random() * 10);
+
+		System.out.println("Koji je kvadrat broja " + broj1 + "?");
+
+		rezultat = Math.pow(broj1, 2);
+
+	}
 }
--- QuizTest.java
+++ QuizTest.java
@@ -1,5 +1,77 @@
 package Mathquiz;
 
+import java.util.Scanner;
+
 public class QuizTest {
 
+	public static void main(String[] args) {
+
+		Quiz kviz = new Quiz();
+		Ispis ispis = new Ispis();
+
+		ispis.ispis();
+		Scanner unos = new Scanner(System.in);
+
+		int unosKorisnika = unos.nextInt();
+
+		switch (unosKorisnika) {
+		case 1:
+			kviz.sabiranje();
+			int rezultat = unos.nextInt();
+			if (rezultat == kviz.getRezultat()) {
+				System.out.println("Cestitamo, pogodili ste rezultat.");
+			} else
+				System.out.println("Nazalost niste pogodili rezultat.");
+
+			break;
+		case 2:
+			kviz.oduzimanje();
+			rezultat = unos.nextInt();
+			if (rezultat == kviz.getRezultat()) {
+				System.out.println("Cestitamo, pogodili ste rezultat.");
+			} else
+				System.out.println("Nazalost niste pogodili rezultat.");
+
+			break;
+		case 3:
+			kviz.mnozenje();
+			rezultat = unos.nextInt();
+			if (rezultat == kviz.getRezultat()) {
+				System.out.println("Cestitamo, pogodili ste rezultat.");
+			} else
+				System.out.println("Nazalost niste pogodili rezultat.");
+
+			break;
+		case 4:
+			kviz.dijeljenje();
+			rezultat = unos.nextInt();
+			if (rezultat == kviz.getRezultat()) {
+				System.out.println("Cestitamo, pogodili ste rezultat.");
+			} else
+				System.out.println("Nazalost niste pogodili rezultat.");
+
+			break;
+		case 5:
+			kviz.korjenovanje();
+			rezultat = unos.nextInt();
+			if (rezultat == kviz.getRezultat()) {
+				System.out.println("Cestitamo, pogodili ste rezultat.");
+			} else
+				System.out.println("Nazalost niste pogodili rezultat.");
+
+			break;
+		case 6:
+			kviz.kvadriranje();
+			rezultat = unos.nextInt();
+			if (rezultat == kviz.getRezultat()) {
+				System.out.println("Cestitamo, pogodili ste rezultat.");
+			} else
+				System.out.println("Nazalost niste pogodili rezultat.");
+
+			break;
+
+		}
+
+		unos.close();
+	}
 }
